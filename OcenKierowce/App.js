25a03eef8c5a5db35975ec721e03d42544ddf1e4
Adam <amitrega01@gmail.com>
2019-04-10 15:01:31 +0200

    added BigButton

--- OcenKierowce/App.js
+++ OcenKierowce/App.js
@@ -1,7 +1,9 @@
 import React, { setGlobal } from 'reactn';
-
+import { createStackNavigator, createAppContainer } from 'react-navigation';
+import FirstRunScreen from './src/screens/FirstRunScreen';
 const MainNavigator = createStackNavigator({
   //TODO EKRANY
+  FirstRun: { screen: FirstRunScreen },
 });
 
 const AppContainer = createAppContainer(MainNavigator);
