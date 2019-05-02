022b76e0f334a97d886208297dee8364b705e2e3
Artur Ulfig <artur.ulfig2@gmail.com>
2019-05-02 18:21:12 +0200

    ZalogujButton

--- OcenKierowce/src/screens/FirstRunScreen.js
+++ OcenKierowce/src/screens/FirstRunScreen.js
@@ -1,20 +1,26 @@
-import React from 'reactn';
-import { Text, View } from 'react-native';
-import Styles from '../consts/Styles';
-import BigButton from '../components/BigButton';
+import React from "reactn";
+import { Text, View } from "react-native";
+import Styles from "../consts/Styles";
+import BigButton from "../components/BigButton";
 export class FirstRunScreen extends React.Component {
   static navigationOptions = {
-    header: null,
+    header: null
   };
 
   render() {
     return (
       <View style={Styles.wrapper}>
         <BigButton
-          color='#151146'
-          title='Zarejestruj się'
-          onPress={() => alert('Rejestracja')}
-          width='80%'
+          color="#151146"
+          title="Zarejestruj się"
+          onPress={() => alert("Rejestracja")}
+          width="80%"
+        />
+        <BigButton
+          color="#00D463"
+          title="Zaloguj się"
+          onPress={() => alert("Logowanie")}
+          width="80%"
         />
       </View>
     );
