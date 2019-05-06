2fb1af109e399fa32d1f4277f2fb517d8eb0f53e
Adam <amitrega01@gmail.com>
2019-05-06 15:31:15 +0200

    link to new screen

--- OcenKierowce/src/screens/FirstRunScreen.js
+++ OcenKierowce/src/screens/FirstRunScreen.js
@@ -1,28 +1,40 @@
-import React from "reactn";
-import { Text, View } from "react-native";
-import Styles from "../consts/Styles";
-import BigButton from "../components/BigButton";
+import React from 'reactn';
+import { Text, View } from 'react-native';
+import Styles from '../consts/Styles';
+import BigButton from '../components/BigButton';
 export class FirstRunScreen extends React.Component {
   static navigationOptions = {
-    header: null
+    header: null,
   };
 
   render() {
     return (
       <View style={Styles.wrapper}>
+        <Text style={Styles.bigText}>OcenKierowce</Text>
         <BigButton
-          color="#151146"
-          title="Zarejestruj się"
-          onPress={() => alert("Rejestracja")}
-          width="80%"
+          color='#151146'
+          title='Zarejestruj się'
+          onPress={() => alert('Rejestracja')}
+          width='60%'
         />
         <BigButton
-          color="#00D463"
-          title="Zaloguj się"
-          onPress={() => alert("Logowanie")}
-          width="80%"
+          color='#00D463'
+          title='Zaloguj się'
+          onPress={() => alert('Logowanie')}
+          width='60%'
         />
-        <BigButton color="#000" title="Anonimowo" width="80%" />
+        <BigButton
+          color='#000'
+          title='Anonimowo'
+          width='60%'
+          onPress={() => {
+            console.log('LOGOWANIE ANONIMOWO');
+            this.setGlobal({
+              anonymous: true,
+            });
+            this.props.navigation.navigate('Home');
+          }}
+        />
       </View>
     );
   }
