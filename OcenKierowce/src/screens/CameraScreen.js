859aa523473e93754c4cc4a7d3bc52a171902e46
Adam <amitrega01@gmail.com>
2019-06-06 10:54:48 +0200

    fixed snap button

--- OcenKierowce/src/screens/CameraScreen.js
+++ OcenKierowce/src/screens/CameraScreen.js
@@ -1,37 +1,40 @@
-import React from "reactn";
-import { Text, View, TouchableOpacity, Image } from "react-native";
-import { Camera, Permissions } from "expo";
+import React from 'reactn';
+import { Text, View, TouchableOpacity, Image } from 'react-native';
+import { Camera, Permissions } from 'expo';
 
 export default class CameraExample extends React.Component {
   state = {
     hasCameraPermission: null,
-    type: Camera.Constants.Type.back
+    type: Camera.Constants.Type.back,
   };
 
+  static navigationOptions = {
+    header: null,
+  };
   async componentDidMount() {
     const { status } = await Permissions.askAsync(Permissions.CAMERA);
     this.setState({
-      hasCameraPermission: status === "granted"
+      hasCameraPermission: status === 'granted',
     });
   }
   snap = async () => {
-    console.log("Button Pressed");
+    console.log('Button Pressed');
     if (this.camera) {
-      console.log("Taking photo");
+      console.log('Taking photo');
       const options = {
         quality: 0,
         base64: true,
         fixOrientation: true,
         exif: true,
-        skipProcessing: true
+        skipProcessing: true,
       };
       await this.camera.takePictureAsync(options).then(photo => {
         photo.exif.Orientation = 1;
 
         this.setGlobal({
-          lastPhoto: photo.uri
+          lastPhoto: photo.uri,
         });
-        this.props.navigation.navigate("NewMessage");
+        this.props.navigation.navigate('NewMessage');
       });
     }
   };
@@ -45,9 +48,8 @@ export default class CameraExample extends React.Component {
       return (
         <View
           style={{
-            flex: 1
-          }}
-        >
+            flex: 1,
+          }}>
           <Camera
             ref={ref => {
               this.camera = ref;
@@ -55,36 +57,31 @@ export default class CameraExample extends React.Component {
             autoFocus={false}
             useCamera2Api={true}
             style={{
-              flex: 1
+              flex: 1,
             }}
-            type={this.state.type}
-          >
+            type={this.state.type}>
             <View
               style={{
                 flex: 1,
-                backgroundColor: "transparent",
-                flexDirection: "row"
-              }}
-            >
+                backgroundColor: 'transparent',
+                flexDirection: 'row',
+                justifyContent: 'center',
+                paddingBottom: 24,
+              }}>
               <TouchableOpacity
                 style={{
-                  flex: 0.1,
-                  alignSelf: "flex-end",
-                  alignItems: "center",
-                  justifyContent: "center"
+                  alignSelf: 'flex-end',
+                  alignItems: 'center',
+                  justifyContent: 'center',
                 }}
-                onPress={() => this.snap()}
-              >
+                onPress={() => this.snap()}>
                 <View
                   style={{
                     width: 60,
                     height: 60,
                     borderWidth: 2,
                     borderRadius: 60,
-                    borderColor: "#FFFFFF",
-                    alignItems: "center",
-                    justifyContent: "center",
-                    marginLeft: 342
+                    borderColor: '#FFFFFF',
                   }}
                 />
               </TouchableOpacity>
